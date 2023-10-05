bc2263f63fd38ab5144db3d7b7d1ea8c01aebf26
Nikita Maslakov <79843027+prot0s34@users.noreply.github.com>
2023-10-05 21:54:36 +0300

    Update wsl-forwarding.ps1

--- wsl-forwarding.ps1
+++ wsl-forwarding.ps1
@@ -1,2 +1,1 @@
-Get-NetIPInterface | where {$_.InterfaceAlias -eq 'vEthernet (WSL)' -or $_.InterfaceAlias -eq 'vEthernet (k8s-switch)'} | Set-NetIPInterface -Forwarding Enabled
-Get-NetIPInterface | where {$_.InterfaceAlias -eq 'vEthernet (WSL)' -or $_.InterfaceAlias -eq 'vEthernet (Default Switch)'} | Set-NetIPInterface -Forwarding Enabled
+Get-NetIPInterface | where {$_.InterfaceAlias -eq 'vEthernet (WSL)' -or $_.InterfaceAlias -eq 'vEthernet (k8s-switch)' -or $_.InterfaceAlias -eq 'vEthernet (Default Switch)'} | Set-NetIPInterface -Forwarding Enabled
