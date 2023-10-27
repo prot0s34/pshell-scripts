2334b8e269433934ec51886c45c1d6e32a9790f9
Nikita Maslakov <79843027+prot0s34@users.noreply.github.com>
2023-10-27 19:36:36 +0300

    revert-hyperv-checkpoint.ps1 - add single confirmation
    add:
    - list of VM's that will be restored
    - single confirm [Y/N] for all hosts

--- revert-hyperv-checkpoint.ps1
+++ revert-hyperv-checkpoint.ps1
@@ -6,15 +6,26 @@ $matchingVMs = Get-VM | Where-Object { $_.Name -like $vmNamePattern }
 if ($matchingVMs.Count -eq 0) {
     Write-Host "No VMs matching the pattern '$vmNamePattern' found."
 } else {
-    foreach ($vm in $matchingVMs) {
-        $checkpoints = Get-VMSnapshot -VMName $vm.Name
-        if ($checkpoints.Count -eq 0) {
-            Write-Host "No checkpoints found for VM $($vm.Name)."
-        } else {
-            $lastCheckpoint = $checkpoints[-1]
-            Write-Host "Reverting VM $($vm.Name) to the last checkpoint: $($lastCheckpoint.Name)..."
-            Restore-VMSnapshot -Name $lastCheckpoint.Name -VMName $vm.Name
-            Write-Host "Revert completed for VM $($vm.Name)."
+    $vmList = $matchingVMs | ForEach-Object { $_.Name }
+
+    Write-Host "The following VMs will be reverted to the last checkpoint:"
+    $vmList | ForEach-Object { Write-Host $_ }
+
+    $confirmation = Read-Host "Revert all matching VMs to the last checkpoint? (Y/N)"
+
+    if ($confirmation -eq "Y") {
+        foreach ($vm in $matchingVMs) {
+            $checkpoints = Get-VMSnapshot -VMName $vm.Name
+            if ($checkpoints.Count -eq 0) {
+                Write-Host "No checkpoints found for VM $($vm.Name)."
+            } else {
+                $lastCheckpoint = $checkpoints[-1]
+                Write-Host "Reverting VM $($vm.Name) to the last checkpoint: $($lastCheckpoint.Name)..."
+                Restore-VMSnapshot -Name $lastCheckpoint.Name -VMName $vm.Name -Confirm:$false
+                Write-Host "Revert completed for VM $($vm.Name)."
+            }
         }
+    } else {
+        Write-Host "Reverting VMs canceled."
     }
 }
